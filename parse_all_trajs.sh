6c6bea83451f426a8a0a18f7f9497b01932c6870
BrunoRosenthal <72097131+BrunoRosenthal@users.noreply.github.com>
2025-03-13 21:23:59 +0000

    Create parse_all_trajs.sh

---
+++ parse_all_trajs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TRAJ_DIR="/Users/bruno/Desktop/diss/src/trajs"  # Folder containing all trajectory files
+SCRIPT="parseTraj.py"  # Python script to run
+
+for traj_file in "$TRAJ_DIR"/*.lammpstrj; do
+    echo "Processing $traj_file..."
+    python "$SCRIPT" "$traj_file"
+done
+
+echo "All trajectory files processed!"
